ce045ce65ec8eb582141a50eae65f572e1d87846
Spiros Kabasakalis <kabasakalis@gmail.com>
2017-08-11 03:00:46 +0300

    Compile with g++

--- cmake/CompileOptions.cmake
+++ cmake/CompileOptions.cmake
@@ -138,7 +138,7 @@ if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU" OR "${CMAKE_CXX_COMPILER_ID}" MATCH
         # -Wmisleading-indentation
 
         # clang
-        -stdlib=libc++
+        # -stdlib=libc++
         # -stdlib=libstdc++
         $<$<CXX_COMPILER_ID:GNU>:
             -Wmaybe-uninitialized
